0eab75516b74aaa43980a57ee25597e89cab479b
DonHundley <112941023+DonHundley@users.noreply.github.com>
2024-03-05 11:48:26 +0100

    week 9 init changes
    removal of console functionality. change of drone.yml to upload to docker.

--- app/Program.cs
+++ app/Program.cs
@@ -1,9 +1,7 @@
 ﻿namespace app;
 
-
 public class Program
 {
-    
     // Represents a calculator service object.
     // This object provides functionality for performing basic mathematical operations.
     private readonly CalculatorService _calculatorService;
@@ -15,87 +13,31 @@ public class Program
         _calculatorService = new CalculatorService(calculator);
     }
 
-    
-    // This method runs a console calculator program.
-    // It prompts the user to enter two numbers and an operation, and then performs the operation and displays the result.
-    // The program continues running until the user enters 'exit' or encounters an error.
+    // This method runs a calculator program.
+    // It uses preset numbers and operation, performs the operation and displays the result.
     public void Run()
     {
-        Console.WriteLine("Console Calculator\n");
-        Console.WriteLine("Enter 'exit' to quit\n");
+        Console.WriteLine("Calculator\n");
 
-        while (true)
+        double n1 = 25;
+        double n2 = 5;
+        string operation = "divide";
+
+        double result;
+
+        try
         {
-            double n1 = GetNumber("Enter first number: ");
-            if (double.IsNaN(n1)) break;
-
-            double n2 = GetNumber("Enter second number: ");
-            if (double.IsNaN(n2)) break;
-
-            string operation = GetOperation();
-            if (operation == null) break;
-
-            double result;
-
-            try
-            {
-                result = _calculatorService.PerformOperation(n1, n2, operation);
-                Console.WriteLine($"Result: {result}\n");
-            }
-            catch (InvalidOperationException ex)
-            {
-                Console.WriteLine(ex.Message);
-            }
+            result = _calculatorService.PerformOperation(n1, n2, operation);
+            Console.WriteLine($"Result: {result}\n");
+        }
+        catch (InvalidOperationException ex)
+        {
+            Console.WriteLine(ex.Message);
         }
 
         Console.WriteLine("Goodbye!");
     }
 
-    
-    // Prompts the user to enter a number and returns the entered number.
-    // Returns the entered number. Returns NaN if the user enters a blank or "exit" as the input.
-    // Returns NaN if the user enters an invalid number.
-    private double GetNumber(string prompt)
-    {
-        while (true)
-        {
-            Console.Write(prompt);
-            var input = Console.ReadLine();
-
-            if (string.IsNullOrWhiteSpace(input) || input.Trim().Equals("exit", StringComparison.OrdinalIgnoreCase))
-            {
-                return double.NaN;
-            }
-
-            if (double.TryParse(input, out var number))
-            {
-                return number;
-            }
-
-            Console.WriteLine("Invalid number. Try again");
-        }
-    }
-
-    
-    // Prompts the user to enter an operation (add, subtract, multiply, divide) and returns the entered operation.
-    // Returns the entered operation.
-    private string GetOperation()
-    {
-        while (true)
-        {
-            Console.Write("Enter operation (add, subtract, multiply, divide): ");
-            var operation = Console.ReadLine();
-
-            if (string.IsNullOrWhiteSpace(operation) || operation.Trim().Equals("exit", StringComparison.OrdinalIgnoreCase))
-            {
-                return null;
-            }
-
-            return operation;
-        }
-    }
-
-    
     // The entry point of the application.
     static void Main(string[] args)
     {
